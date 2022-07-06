d974ded49c65942c3c877a987a9d1456d413e867
dPaul <paullai0201@gmail.com>
2022-07-06 22:39:51 +0800

    fix(處理對於String類型返回值的異常):

--- src/main/java/org/dpaul/template/springboot/config/WebServerConfig.java
+++ src/main/java/org/dpaul/template/springboot/config/WebServerConfig.java
@@ -3,15 +3,24 @@ package org.dpaul.template.springboot.config;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
-public class WebServerConfig {
+public class WebServerConfig implements WebMvcConfigurer {
 	/**
 	 * @return 消息轉換器
 	 */
 	@Bean
 	public HttpMessageConverters fastJsonHttpMessageConverters() {
 		return new HttpMessageConverters(new MappingJackson2HttpMessageConverter());
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		converters.add(0, new MappingJackson2HttpMessageConverter());
 	}
 }
