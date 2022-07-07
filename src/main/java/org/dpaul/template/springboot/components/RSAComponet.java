04ebdab39b54a67137381c029fd1fb1ba8f14a82
dPaul <paullai0201@gmail.com>
2022-07-08 03:02:56 +0800

    feat(添加RSA构建方法):

--- src/main/java/org/dpaul/template/springboot/components/RSAComponet.java
+++ src/main/java/org/dpaul/template/springboot/components/RSAComponet.java
@@ -5,6 +5,7 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.crypto.asymmetric.KeyType;
 import cn.hutool.crypto.asymmetric.RSA;
 import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.core.io.Resource;
@@ -15,20 +16,25 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.security.PrivateKey;
-import java.security.PublicKey;
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 @Component
 @Scope
+@Slf4j
 public class RSAComponet {
 
 	private final RSA rsa = new RSA();
-	@Value("classpath:REAKeys/N.txt")
+	@Value("classpath:RSAKeys/N.txt")
 	private Resource N;
-	@Value("classpath:REAKeys/e.txt")
+	@Value("classpath:RSAKeys/e.txt")
 	private Resource e;
-	@Value("classpath:REAKeys/d.txt")
+	@Value("classpath:RSAKeys/d.txt")
 	private Resource d;
+
+	@Value("classpath:RSAKeys/secret.txt")
+	private Resource secret;
 
 	@PostConstruct
 	public void init() {
@@ -48,6 +54,24 @@ public class RSAComponet {
 		final byte[] bytes = StrUtil.bytes(str, StandardCharsets.UTF_8);
 		byte[] encrypt = rsa.encrypt(bytes, KeyType.PrivateKey);
 		return HexUtil.encodeHexStr(encrypt);
+	}
+
+	public void generateKey(String secret, int keySize) throws NoSuchAlgorithmException {
+		int DEFAULT_KEY_SIZE = 4096;
+		String KEY_ALGORITHM = "RSA";
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+		keyPairGenerator.initialize(Math.max(keySize, DEFAULT_KEY_SIZE), new SecureRandom(secret.getBytes()));
+		final KeyPair keyPair = keyPairGenerator.genKeyPair();
+		PrivateKey privateKey = keyPair.getPrivate();
+		PublicKey publicKey = keyPair.getPublic();
+		RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) privateKey;
+		RSAPublicKey rsaPublicKey = (RSAPublicKey) publicKey;
+		final String N = rsaPrivateKey.getModulus().toString();
+		final String d = rsaPrivateKey.getPrivateExponent().toString();
+		final String e = rsaPublicKey.getPublicExponent().toString();
+		log.info("N : " + N);
+		log.info("d : " + d);
+		log.info("e : " + e);
 	}
 
 	/**
@@ -73,7 +97,7 @@ public class RSAComponet {
 
 	private BigInteger Str2BigInt(Resource resource) {
 		try {
-			return new BigInteger(Files.readAllLines(resource.getFile().toPath()).get(0));
+			return new BigInteger(new String(Files.readAllBytes(resource.getFile().toPath()), StandardCharsets.UTF_8));
 		} catch (IOException ex) {
 			return new BigInteger("0");
 		}
